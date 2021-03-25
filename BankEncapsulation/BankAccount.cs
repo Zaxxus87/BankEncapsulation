600c5249eb67c76746a7bba55a58a4efeaa822e1
Jeffrey Baker <jeffrey1871@gmail.com>
2021-03-24 22:42:45 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount()
+        {
+            balance = 0.0;
+        }
+
+        public void Deposit(double amt)
+        {
+            balance = amt;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount newAccount = new BankAccount();
+            newAccount.Deposit(3.50);
+            Console.WriteLine(newAccount.GetBalance());
+        }
+    }
+}
